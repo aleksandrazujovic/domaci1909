15270cab09e792392ba44777645ef753a34c60c2
aleksandrazujovic <aleksandrazujovic997@gmail.com>
2022-09-25 00:23:21 +0200

    My third commit

---
+++ src/Class3.java
@@ -0,0 +1,15 @@
+public class Class3 {
+
+    public static void main(String[] args) {
+
+
+        String ime = "Aleksandra";
+        String prezime = "Zujovic";
+
+        System.out.println(ime + " " + prezime);
+
+
+    }
+
+
+}
